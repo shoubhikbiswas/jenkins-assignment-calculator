4ab20c699dae4d19267dffeb9d8eb051958816b9
shoubhikbiswas <shoubhiksukdev.biswas@wissen.com>
2021-05-06 15:07:28 +0530

    Changes in Application.java

--- src/main/java/com/jenkins/assignment/Application.java
+++ src/main/java/com/jenkins/assignment/Application.java
@@ -2,15 +2,20 @@ package com.jenkins.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Application.class);
+	}
 }
